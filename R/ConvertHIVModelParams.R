0c4c669ed2f92f96fe2ae3766180164a07d18af3
Daniel Lewandowski <daniel@nextpagesoft.net>
2021-04-05 10:12:55 +0200

    Release version 1.9.52

--- R/ConvertHIVModelParams.R
+++ R/ConvertHIVModelParams.R
@@ -4,8 +4,9 @@ ConvertHIVModelParams <- function(
   inputname
 ) {
   intervals <- ConvertListToDt(x$timeIntervals)
-  intervals[, endYear := NULL]
+  # intervals[, endYear := NULL]
   setnames(intervals, new = Capitalize(colnames(intervals)))
+  setcolorder(intervals, c('StartYear', 'EndYear', 'Jump', 'DiffByCD4', 'ChangeInInterval'))
 
   variableCodeMapping <- list(
     G = 'Gender',
@@ -38,18 +39,18 @@ ConvertHIVModelParams <- function(
 
   params <- list(
     Params = list(
-      ModelMinYear = x$minYear,
-      ModelMaxYear = x$maxYear,
-      FitPosMinYear = x$minFitPos,
-      FitPosMaxYear = x$maxFitPos,
-      FitPosCD4MinYear = x$minFitCD4,
-      FitPosCD4MaxYear = x$maxFitCD4,
-      FitAIDSMinYear = x$minFitAIDS,
-      FitAIDSMaxYear = x$maxFitAIDS,
-      FitAIDSPosMinYear = x$minFitHIVAIDS,
-      FitAIDSPosMaxYear = x$maxFitHIVAIDS,
+      ModelMinYear = as.numeric(x$minYear),
+      ModelMaxYear = as.numeric(x$maxYear),
+      FitPosMinYear = as.numeric(x$minFitPos),
+      FitPosMaxYear = as.numeric(x$maxFitPos),
+      FitPosCD4MinYear = as.numeric(x$minFitCD4),
+      FitPosCD4MaxYear = as.numeric(x$maxFitCD4),
+      FitAIDSMinYear = as.numeric(x$minFitAIDS),
+      FitAIDSMaxYear = as.numeric(x$maxFitAIDS),
+      FitAIDSPosMinYear = as.numeric(x$minFitHIVAIDS),
+      FitAIDSPosMaxYear = as.numeric(x$maxFitHIVAIDS),
       FullData = x$fullData,
-      ModelNoKnots = x$knotsCount,
+      ModelNoKnots = as.integer(x$knotsCount),
       StartIncZero = x$startIncZero,
       MaxIncCorr = x$maxIncCorr,
       FitDistribution = x$distributionFit,

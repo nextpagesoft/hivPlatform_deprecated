46a10697ff3d25ae9a026f8ff72f5d18bb0a957d
Daniel Lewandowski <daniel@nextpagesoft.net>
2021-11-03 22:39:19 +0100

    Release version 2.0.4

--- app.R
+++ app.R
@@ -2,6 +2,7 @@
 library(data.table)
 
 # Load hivPlatform package
+try(pkgload::unload(), silent = TRUE)
 pkgload::load_all(path = '.', export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
 
 options(shiny.maxRequestSize = 100 * 1024^2)

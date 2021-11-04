19d924c7ad59f9b885628e6e7a1c4996e8cd21b1
Daniel Lewandowski <daniel@nextpagesoft.net>
2021-11-04 19:55:41 +0100

    Show column type of case-based data attributes

--- app.R
+++ app.R
@@ -3,7 +3,7 @@ library(data.table)
 
 # Load hivPlatform package
 try(pkgload::unload(), silent = TRUE)
-pkgload::load_all(path = '.', export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+pkgload::load_all(path = '.', export_all = TRUE, helpers = FALSE, attach_testthat = FALSE)
 
 options(shiny.maxRequestSize = 100 * 1024^2)
 shiny::shinyApp(AppUI, AppServer)
